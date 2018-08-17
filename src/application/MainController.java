84043534a3bdf41248d44530aeaa4951cf372676
ibrahim <ibrahim@ibrahim-PC>
2018-08-18 11:03:47 +1200

    Before Renaming Preprocessing

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -12,16 +12,23 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.Random;
+import java.util.Set;
 
 import javax.imageio.ImageIO;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.geometry.Bounds;
@@ -32,7 +39,9 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Data;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceDialog;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
 import javafx.scene.control.ScrollPane;
@@ -40,6 +49,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -67,10 +77,15 @@ import org.opencv.core.MatOfByte;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
+import com.sun.deploy.uitoolkit.impl.fx.ui.FXConsole;
 
 import java.lang.reflect.Field;
+import java.net.NetworkInterface;
+
 import org.opencv.highgui.HighGui;
 
+
+import javafx.collections.FXCollections;
 public class MainController {
 	//UI ELEMENTS
 	@FXML private  Label MainLabel;
@@ -82,7 +97,8 @@ public class MainController {
 	@FXML private ScrollPane ImgScrollPane;
 	@FXML private GridPane ConvMaskGridP; 
 	@FXML private HBox ConvBox;
-
+	@FXML private CheckBox ToBufferImage;
+	@FXML private ComboBox<String> FeaturesDB;
     
 	private Main main;
 	private DataSetsLoader myDataLoader 	= new DataSetsLoader();
@@ -98,16 +114,25 @@ public class MainController {
 	public Mat imageBufA;
 	public Mat imageBufB;
 	public Mat bufferImage;
-	
+	Map<String, Runnable> features = new HashMap<>();
 	@SuppressWarnings("static-access")
 	public void setMain(Main main) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		
+//		FeaturesDP.getChildrenUnmodifiable().add();
 		this.main		= main;
+
+		FeaturesDB.getItems().addAll("featureA","featureB","featureC","symmetryX");
+		FeaturesDB.setValue("featureA");
+		features.put("featureA", () -> featureA());	
+		features.put("featureB", () -> featureB());	
+		features.put("featureC", () -> featureC());	
 		
 //		loadEdgeDetectionImage();
 //		test();
 //		applyConvolutionOperator();
+//		System.exit(0);
+		loadRandomFaceImage();
+//		features.get("FeatureA").run();
 //		System.exit(0);
 	}
 	
@@ -181,6 +206,192 @@ public class MainController {
 //		convolutionKernel.put(key, value);
 	}
 	
+	public void threshold() {
+		ToBufferImage.setSelected(true);
+		meanFilter();
+		int threshold					= getUserInt("40", "WhiteThreshold", "Levels of White To Keep", "intensity<x then = 0");
+		bufferImage						= new Mat(loadedImage.height(), loadedImage.width(), 6);
+		double newPixel;
+		for (int col=0; col<loadedImage.size().width;col++) {
+			for (int row=0; row<loadedImage.size().height;row++) {
+				newPixel				= loadedImage.get(row, col)[0];
+				if(Math.round(newPixel)<threshold) {
+					newPixel			= 0;
+				}else {
+					newPixel			=255;
+				}
+				double[] p				= {newPixel,newPixel,newPixel};
+				bufferImage.put(row, col, p);
+			}			
+		}
+		if (ToBufferImage.isSelected()) {
+			loadedImage					= bufferImage.clone();
+		}		
+		showLoadedImageInUi(bufferImage);		
+		
+	}
+	public void medianFilter() {
+		int maskSize					= getUserInt("3", "Kernel Size", "3x3 or 5x5", "Please provide ONE Integer");
+		ImageConvolutionMask mask 		= new ImageConvolutionMask(loadedImage, loadedImage.size().width, loadedImage.size().height, maskSize);
+		mask.calculateRelPixelPositionsInMask();		
+		mask.loadedImage				= loadedImage;
+		mask.convolutionKernel			= convolutionKernel;
+		bufferImage						= new Mat(loadedImage.height(), loadedImage.width(), 6);
+		double newPixel;
+		for (int col=0; col<loadedImage.size().width;col++) {
+			for (int row=0; row<loadedImage.size().height;row++) {
+				mask.pixelX =col;
+				mask.pixelY = row;
+				mask.calculateAbsPixelPositionsInMask();
+//				System.out.println(mask.pixelsUnderMask);
+				newPixel				= mask.calculateMedianFilterValue();
+				double[] p				= {newPixel,newPixel,newPixel};
+				bufferImage.put(row, col, p);
+			}			
+		}
+		if (ToBufferImage.isSelected()) {
+			loadedImage					= bufferImage.clone();
+		}		
+		showLoadedImageInUi(bufferImage);		
+	}
+	
+	public void meanFilter() {
+		int maskSize					= getUserInt("3", "Kernel Size", "3x3 or 5x5", "Please provide ONE Integer");
+		ImageConvolutionMask mask 		= new ImageConvolutionMask(loadedImage, loadedImage.size().width, loadedImage.size().height, maskSize);
+		mask.calculateRelPixelPositionsInMask();		
+		mask.loadedImage				= loadedImage;
+		mask.convolutionKernel			= convolutionKernel;
+		bufferImage						= new Mat(loadedImage.height(), loadedImage.width(), 6);
+		double newPixel;
+		for (int col=0; col<loadedImage.size().width;col++) {
+			for (int row=0; row<loadedImage.size().height;row++) {
+				mask.pixelX =col;
+				mask.pixelY = row;
+				mask.calculateAbsPixelPositionsInMask();
+				newPixel				= mask.calculateMeanFilterValue();
+				double[] p				= {newPixel,newPixel,newPixel};
+				bufferImage.put(row, col, p);
+			}
+		}
+		if (ToBufferImage.isSelected()) {
+			loadedImage					= bufferImage.clone();
+		}		
+		showLoadedImageInUi(bufferImage);		
+	}
+	
+
+	
+	public void checkSymmetryX() {
+		ImageConvolutionMask mask 		= new ImageConvolutionMask(loadedImage, loadedImage.size().width, loadedImage.size().height, 3);
+		mask.loadedImage				= loadedImage;
+		updateStatusText("SymmetryX score " +mask.calculateSymmetryX());
+		
+	}
+	public void test5() {
+		
+		path							= System.getProperty("user.dir").replace('\\', '/') + "/face00008.pgm";
+//		path							= System.getProperty("user.dir").replace('\\', '/') + "/test-pattern.tif";
+		loadedImage						= Imgcodecs.imread(path);
+		currentZoomFactor				= 10.0f;
+		System.out.println(loadedImage);
+			
+		
+
+//		mask.convolutionKernel			= convolutionKernel;
+		bufferImage						= new Mat(loadedImage.height(), loadedImage.width(), 0);
+//		highlightRect(6,2,6,9);
+
+	}
+
+	public void highlightRect(int x, int y, int width, int height) {
+		ImageConvolutionMask mask 		= new ImageConvolutionMask(loadedImage, loadedImage.size().width, loadedImage.size().height, 3);
+		mask.loadedImage				= loadedImage;		
+		LinkedHashMap<ArrayList<Integer>,Double> pixelsInRect = mask.getRect(x, y, width, height);		
+		double totalIntensity			= mask.computeAverageForRect(pixelsInRect);
+		System.out.println(pixelsInRect);
+		for (ArrayList<Integer> pixelPos : pixelsInRect.keySet()) {
+			double [] p = {255,0,0};
+			loadedImage.put(pixelPos.get(1), pixelPos.get(0), 255);
+		}
+		showLoadedImageInUi(loadedImage);
+		updateStatusText("Average Intensity In Rect = "+totalIntensity);
+	}
+	public void test4() {
+		path							= System.getProperty("user.dir").replace('\\', '/') + "/face00008.pgm";;
+		loadedImage						= Imgcodecs.imread(path);
+//		currentZoomFactor				=10.0f;
+		System.out.println(loadedImage);
+		showLoadedImageInUi(loadedImage);	
+		
+		localBinaryPattern();		
+		
+		
+	}
+
+	public void localBinaryPattern() {
+		int maskSize					= getUserInt("3", "MaskSize", "3x3 5x5...etc", "Please Enter One Integer");
+		ImageConvolutionMask mask 		= new ImageConvolutionMask(loadedImage, loadedImage.size().width, loadedImage.size().height, maskSize);
+		mask.calculateRelPixelPositionsInMask();
+//		System.out.println(mask.convolutionKernel);
+//		getConvolutionKernelFromUI();
+		mask.loadedImage				= loadedImage;
+//		mask.convolutionKernel			= convolutionKernel;
+		bufferImage						= new Mat(loadedImage.height(), loadedImage.width(), 0);
+		
+//		System.out.println(loadedImage.size().width);
+		double newPixel;
+		for (int col=0; col<loadedImage.size().width;col++) {
+			for (int row=0; row<loadedImage.size().height;row++) {
+				
+//				System.out.println(loadedImage.get(row, col)[2]);
+				mask.pixelX =col;
+				mask.pixelY = row;
+				mask.calculateAbsPixelPositionsInMask();
+//				System.out.println(mask.pixelsUnderMask);
+				newPixel				= mask.calculateLBPPixelValue();
+				bufferImage.put(row, col, newPixel);
+//				System.out.println(newPixel+"...."+bufferImage.get(row, col)[0]);
+				
+			}
+
+		}
+		if (ToBufferImage.isSelected()) {
+			loadedImage					= bufferImage.clone();
+		}		
+		showLoadedImageInUi(bufferImage);			
+	}
+	public void test3() {
+		loadEdgeDetectionImage();
+		String [] kernel = {"-1","0","1","-2","0","2","-1","0","1"};
+//		String [] kernel = {"1","2","1","0","0","0","-1","-2","-1"};
+		
+		fillKernel(kernel);
+		getConvolutionKernelFromUI();
+		System.out.println(convolutionKernel);
+		System.out.println(convolutionKernel.size());
+		ImageConvolutionMask mask 		= new ImageConvolutionMask(loadedImage, loadedImage.size().width, loadedImage.size().height, 3);
+		mask.calculateRelPixelPositionsInMask();
+		getConvolutionKernelFromUI();
+		mask.loadedImage				= loadedImage;
+		mask.convolutionKernel			= convolutionKernel;
+		bufferImage						= new Mat(loadedImage.height(), loadedImage.width(), 6);
+//		System.out.println(loadedImage.size().width);
+		double newPixel;
+		mask.pixelX =18;
+		mask.pixelY = 14;
+				
+		mask.calculateAbsPixelPositionsInMask();
+		System.out.println(mask.pixelsUnderMask);
+		newPixel				= mask.calculateNewPixelValue();
+		System.out.println(mask.pixelsUnderMask);
+		System.out.println("NEW PIXEL ="+newPixel);
+		double[] p					= {newPixel,newPixel,newPixel};
+		double[]d ={255,0,0};
+		bufferImage.put(14, 18, p);
+		bufferImage.put(13, 18, d);
+		showLoadedImageInUi(bufferImage);
+		System.out.println(bufferImage.get(14, 18)[0]);
+	}
 	public void loadImageFromDisk() {
         JFileChooser chooser = new JFileChooser();
         int returnName = chooser.showOpenDialog(null);
@@ -215,7 +426,8 @@ public class MainController {
 		
 	}
 	
-	public void sobelX() {
+	
+	public void fillKernel(String[]kernel) {
 		for ( Node valueUI :ConvMaskGridP.getChildren()) {			
 			if (valueUI.idProperty().getValue() != null) {
 //				System.out.print(valueUI.idProperty().getValue()+" ");
@@ -224,39 +436,79 @@ public class MainController {
 				TextField tf		= (TextField)valueUI;
 //				System.out.println(id);
 				if (id.equals("Conv00TF")) {
-					tf.setText("-1");
+					tf.setText(kernel[0]);
 				}
 				if (id.equals("Conv01TF")) {
-					tf.setText("0");
+					tf.setText(kernel[1]);
 				}	
 				if (id.equals("Conv02TF")) {
-					tf.setText("1");
+					tf.setText(kernel[2]);
 				}	
 				if (id.equals("Conv10TF")) {
-					tf.setText("-2");
+					tf.setText(kernel[3]);
 				}	
 				if (id.equals("Conv11TF")) {
-					tf.setText("0");
+					tf.setText(kernel[4]);
 				}
 				if (id.equals("Conv12TF")) {
-					tf.setText("2");
+					tf.setText(kernel[5]);
 				}				
 				if (id.equals("Conv20TF")) {
-					tf.setText("-1");
+					tf.setText(kernel[6]);
 				}
 				if (id.equals("Conv21TF")) {
-					tf.setText("0");
+					tf.setText(kernel[7]);
 				}				
 				if (id.equals("Conv22TF")) {
-					tf.setText("1");
+					tf.setText(kernel[8]);
 				}				
 				
 			}
 		}	
+		
+	}
+	public void featureA() {
+		//Cheeks High lighter
+		String [] kernel = {"-1","-1","-1","0","0","0","1","1","1"};
+//		String [] kernel = {"-4","-4","-4","1","1","1","3","3","3"};
+		fillKernel(kernel);
+		applyConvolutionOperator();
+		
+	}		
+	public void featureC() {
+		//Cheeks High lighter
+		String [] kernel = {"-2","-2","0","-2","0","2","0","2","2"};
+		fillKernel(kernel);
+		applyConvolutionOperator();
+		
+	}	
+	public void featureB() {
+		String [] kernel = {"0","-1","0","-1","6","-1","0","1","0"};
+		fillKernel(kernel);
+		applyConvolutionOperator();
+		
+	}
+	public void sobelX() {		
+		String [] kernel = {"-1","0","1","-2","0","2","-1","0","1"};
+		fillKernel(kernel);
 		applyConvolutionOperator();
 		imageBufA 					= bufferImage.clone();
 	}	
 	
+
+	public void denoise() {		
+		String [] kernel = {"1/9","1/9","1/9","1/9","1/9","1/9","1/9","1/9","1/9"};
+		fillKernel(kernel);
+		applyConvolutionOperator();
+		imageBufA 					= bufferImage.clone();
+	}	
+	
+	public void enhance() {		
+		String [] kernel = {"0","-1","0","-1","5","-1","0","-1","0"};
+		fillKernel(kernel);
+		applyConvolutionOperator();
+		imageBufA 					= bufferImage.clone();
+	}	
 	
 	public void sobelY() {
 		for ( Node valueUI :ConvMaskGridP.getChildren()) {			
@@ -300,28 +552,51 @@ public class MainController {
 		imageBufB							= bufferImage.clone();
 	}
 	
+	public int getUserInt(String defaultValue, String title, String header, String context) {
+		TextInputDialog dialog = new TextInputDialog(defaultValue);
+		dialog.setTitle(title);
+		dialog.setHeaderText(header);
+		dialog.setContentText(context);
+
+		// Traditional way to get the response value.
+		Optional<String> result = dialog.showAndWait();
+		if (result.isPresent()){
+			return Integer.parseInt(result.get());
+		}
+		return 0;
+	}
 	public void sobelOperator() {
-		sobelX();
-//		saveSnapShot();
-		sobelY();
-//		saveSnapShot();
-		for (int col=0; col<loadedImage.size().width;col++) {
-			for (int row=0; row<loadedImage.size().height;row++) {
-				
-				double gX					= imageBufA.get(row, col)[0];
-				double gY					= imageBufB.get(row, col)[0];
-				double newPixel				= Math.sqrt((Math.pow(gX, 2)+Math.pow(gY, 2)));
-//				if (newPixel<100) {
-//					newPixel				= 0;
-//				}
-				double[] p					= {newPixel,newPixel,newPixel};
-				bufferImage.put(row, col, p);
-				
-			}
+		TextInputDialog dialog = new TextInputDialog("30");
+		dialog.setTitle("Edge Detection Threshold");
+		dialog.setHeaderText("Threshold");
+		dialog.setContentText("Intensity Threshold = ");
+
+		// Traditional way to get the response value.
+		Optional<String> result = dialog.showAndWait();
+		if (result.isPresent()){
 			
-		}		
-		showLoadedImageInUi(bufferImage);
-		
+					
+			sobelX();
+	//		saveSnapShot();
+			sobelY();
+	//		saveSnapShot();
+			for (int col=0; col<loadedImage.size().width;col++) {
+				for (int row=0; row<loadedImage.size().height;row++) {
+					
+					double gX					= imageBufA.get(row, col)[0];
+					double gY					= imageBufB.get(row, col)[0];
+					double newPixel				= Math.sqrt((Math.pow(gX, 2)+Math.pow(gY, 2)));
+					if (newPixel<Double.parseDouble(result.get())) {
+						newPixel				= 0;
+					}
+					double[] p					= {newPixel,newPixel,newPixel};
+					bufferImage.put(row, col, p);
+					
+				}
+				
+			}		
+			showLoadedImageInUi(bufferImage);
+		}
 	}
 	public void writeImage() {
 		File f 								= new File(path);
@@ -341,6 +616,47 @@ public class MainController {
 		updateStatusText("Reloaded Image From Disk !!!");
 		
 	}
+	public void loadRandomFaceImage() {
+		currentZoomFactor				=10.0f;
+		String directory				=System.getProperty("user.dir").replace('\\', '/') + "/mit-cbcl-faces-balanced/train/face";
+		File aDirectory 				= new File(directory);
+		String[] filesInDir 			= aDirectory.list();
+		int randomIndex					= new Random().nextInt(filesInDir.length);
+		path							= directory +"/"+filesInDir[randomIndex];
+		System.out.println(path);
+		loadedImage						= Imgcodecs.imread(path);
+		System.out.println(loadedImage);
+		System.out.println(loadedImage.channels());
+		
+		previewFeatureTest();
+		showLoadedImageInUi(loadedImage);
+//			sobelY();
+//			writeImage();
+		
+	}
+
+	public void previewFeatureTest() {
+		ToBufferImage.setSelected(true);
+		enhance();
+		applyFeatureFromDropBox();
+//		highlightRect(6,4,6,6);
+//		highlightRect(7,1,4,8);
+	}
+	
+	public void loadRandomNoneFaceImage() {
+		String directory				=System.getProperty("user.dir").replace('\\', '/') + "/mit-cbcl-faces-balanced/train/non-face";
+		File aDirectory 				= new File(directory);
+		String[] filesInDir 			= aDirectory.list();
+		int randomIndex					= new Random().nextInt(filesInDir.length);
+		path							= directory +"/"+filesInDir[randomIndex];
+		System.out.println(path);
+		loadedImage						= Imgcodecs.imread(path);
+		System.out.println(loadedImage);
+		System.out.println(loadedImage.channels());		
+		previewFeatureTest();
+		showLoadedImageInUi(loadedImage);
+		
+	}	
 	public void test() {
 		String directory				="C:\\Users\\ibrahim\\Pictures\\Brian";
 		File aDirectory 				= new File(directory);
@@ -392,13 +708,49 @@ public class MainController {
 	}
 	public void loadEdgeDetectionImage() {	
 		effectIterations				=0;
-		loadedImage						= Imgcodecs.imread(System.getProperty("user.dir").replace('\\', '/') + "/test-pattern.tif");
+		path							= System.getProperty("user.dir").replace('\\', '/') + "/test-pattern.tif";
+		loadedImage						= Imgcodecs.imread(path);
 		showLoadedImageInUi(loadedImage);
 		System.out.println(SourceImgGRP.getChildren());
 		System.out.println(loadedImage);
-		updateStatusText("Loaded test-pattern.tif !!!");
+		updateStatusText("Loaded test-pattern.tif!");
 	}
 	
+	public void loadEnhanceImage() {	
+		effectIterations				=0;
+		path							= System.getProperty("user.dir").replace('\\', '/') + "/blurry-moon.tif";
+		loadedImage						= Imgcodecs.imread(path);
+		showLoadedImageInUi(loadedImage);
+		System.out.println(SourceImgGRP.getChildren());
+		System.out.println(loadedImage);
+		updateStatusText("Loaded blurry-moon.tif!");
+	}	
+	
+	public void loadHubbleImage() {	
+		effectIterations				=0;
+		path							= System.getProperty("user.dir").replace('\\', '/') + "/hubble.tif";
+		loadedImage						= Imgcodecs.imread(path);
+		showLoadedImageInUi(loadedImage);
+		System.out.println(SourceImgGRP.getChildren());
+		System.out.println(loadedImage);
+		updateStatusText("Loaded hubble.tif!");
+	}		
+	
+	public void loadDenoiseImage() {	
+		effectIterations				=0;
+		path							= System.getProperty("user.dir").replace('\\', '/') + "/ckt-board-saltpep.tif";
+		loadedImage						= Imgcodecs.imread(path);
+		showLoadedImageInUi(loadedImage);
+		System.out.println(SourceImgGRP.getChildren());
+		System.out.println(loadedImage);
+		updateStatusText("Loaded ckt-board-saltpep.tif!");
+	}	
+	
+	
+	public void applyFeatureFromDropBox() {
+		reloadImageFromDisk();
+		features.get(FeaturesDB.getValue()).run();
+	}
 	public void applyConvolutionOperator() {
 		effectIterations				+=1;
 		ImageConvolutionMask mask 		= new ImageConvolutionMask(loadedImage, loadedImage.size().width, loadedImage.size().height, 3);
@@ -406,7 +758,8 @@ public class MainController {
 		getConvolutionKernelFromUI();
 		mask.loadedImage				= loadedImage;
 		mask.convolutionKernel			= convolutionKernel;
-		bufferImage						= loadedImage.clone();
+		bufferImage						= new Mat(loadedImage.height(), loadedImage.width(), 6);
+		
 		System.out.println(loadedImage.size().width);
 		double newPixel;
 		for (int col=0; col<loadedImage.size().width;col++) {
@@ -419,12 +772,14 @@ public class MainController {
 //				System.out.println(mask.pixelsUnderMask);
 				newPixel				= mask.calculateNewPixelValue();
 				double[] p					= {newPixel,newPixel,newPixel};
-				bufferImage.put(row, col, p);
+				bufferImage.put(row, col, newPixel);
 				
 			}
-			
+
 		}
-		
+		if (ToBufferImage.isSelected()) {
+			loadedImage					= bufferImage.clone();
+		}		
 		showLoadedImageInUi(bufferImage);
 
 		updateStatusText("Applied Convolution Kernel "+effectIterations+ " Times");
--- src/model/ImageConvolutionMask.java
+++ src/model/ImageConvolutionMask.java
@@ -1,8 +1,11 @@
 package model;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 import org.opencv.core.Mat;
 
@@ -60,6 +63,158 @@ public class ImageConvolutionMask {
 		
 	}
 	
+	public float map(double totalPixelValue, float in_min, float in_max, float out_min, float out_max)
+	{
+	  return (float) ((totalPixelValue - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+	}
+	
+	public double calculateMeanFilterValue() {
+		double totalPixelValue	= 0.0f;
+		for (ArrayList<Integer> pixel: pixelsUnderMask.keySet()) {
+			float oldPixelValue = pixelsUnderMask.get(pixel);
+			totalPixelValue += oldPixelValue;
+//			pixelsUnderMask.put(pixel, newPixelValue);
+		}		
+		totalPixelValue			= totalPixelValue/pixelsUnderMask.size();
+		return totalPixelValue;
+	}
+	
+	public double calculateMedianFilterValue() {
+		
+		ArrayList <Integer> pixels = new ArrayList<>();		
+		for (ArrayList<Integer> pixel:pixelsUnderMask.keySet()) {
+//			pixelValues[counter]=Math.round(pixelsUnderMask.get(pixel));
+			pixels.add(Math.round(pixelsUnderMask.get(pixel)));			
+			}
+		Object[]pixelValues = pixels.toArray() ;
+		Arrays.sort(pixels.toArray());
+		int middle = pixelValues.length/2;
+		int medianValue = 0; //declare variable 
+		if (pixelValues.length%2 == 1) { 
+		    medianValue = (int) pixelValues[middle];
+//			System.out.println(pixels+"\n"+medianValue);
+		}
+		else {
+		   medianValue = ((int)pixelValues[middle-1] + (int)pixelValues[middle]) / 2;
+		}
+		return medianValue;
+		
+	}
+	public ArrayList<Integer> range(int start, int stop)
+	{
+	   ArrayList<Integer> result 		= new ArrayList<>();
+
+	   for(int i=0;i<stop-start;i++)
+	      result.add(start+i);
+
+	   return result;
+	}	
+	public ArrayList<ArrayList<Integer>> getSymmetryXhalves() {
+		int middlePixel				= loadedImage.width()/2;
+		int remainder				= loadedImage.width()%2;
+		ArrayList<ArrayList<Integer>> imageHalves = new ArrayList<>();
+		ArrayList<Integer> leftSide = range(0,middlePixel);
+		ArrayList<Integer>rightSide;
+		if (remainder==1) {
+			rightSide				= range(middlePixel+1,loadedImage.width());	
+		}else {
+			rightSide				= range(middlePixel,loadedImage.width());
+		}
+		Collections.reverse(rightSide);
+		System.out.println(remainder);
+		System.out.println("Left  side column Count "+leftSide);
+		System.out.println("Right side column Count "+rightSide);
+		imageHalves.add(leftSide);
+		imageHalves.add(rightSide);
+		return imageHalves;
+				
+	}
+	public double computeAverageForRect(LinkedHashMap<ArrayList<Integer>,Double> pixels) {
+		double average					= 0;
+		for (ArrayList<Integer> pixelPos :pixels.keySet()) {
+			double pixelIntensity		= pixels.get(pixelPos);
+			if (pixelIntensity<0)pixelIntensity=0;
+			if (pixelIntensity>255)pixelIntensity=255;
+			pixels.put(pixelPos, pixelIntensity);
+			average						+= pixelIntensity;
+		}
+		System.out.print("Total Intensity = "+ average +" / "+pixels.size()+" = ");
+		average							= average/pixels.size();
+		System.out.println(average);
+		return average;
+	}
+	public LinkedHashMap<ArrayList<Integer>,Double> getRect(int x, int y, int width, int height){
+		 LinkedHashMap<ArrayList<Integer>,Double> pixels	= new LinkedHashMap<>();
+		
+		for (int col=x; col<=width+x;col++) {
+			for (int row=y; row<=height+y;row++) {
+				ArrayList<Integer> pixelPos = new ArrayList<>();
+				pixelPos.add(col);
+				pixelPos.add(row);
+				pixels.put(pixelPos, loadedImage.get(row, col)[0]);
+			}
+		}
+		return pixels;
+	}
+	public double calculateSymmetryX() {
+		
+		ArrayList<Double> leftSide	= new ArrayList<>();
+		ArrayList<Double> rightSide	= new ArrayList<>();
+		ArrayList<ArrayList<Integer>> imageHalves = getSymmetryXhalves();
+		
+		// GENERATING TWO SIDES LISTS TO COMPARE
+		for (ArrayList<Integer> half: imageHalves) {
+			int index				= imageHalves.indexOf(half);
+			for (int col:half) {
+				for (int row=0; row<loadedImage.size().height;row++) {
+					if (index==0) {
+						leftSide.add(loadedImage.get(row, col)[0]);
+					}
+					else {
+						rightSide.add(loadedImage.get(row, col)[0]);
+					}
+				}
+			}
+		}
+		// COMPARING TWO SIDES TO GENERATE AS SYMMETRY SCORE
+		double symmetryScore 		= calculateSymmetryScoreForQuads(leftSide, rightSide);
+//		System.out.println("Left Side "+leftSide.size());
+//		System.out.println("Right Side "+rightSide.size());
+		return symmetryScore;
+	}
+
+
+	public double calculateSymmetryScoreForQuads(ArrayList<Double> quad1, ArrayList<Double> quad2) {
+		int pixelCounter			= 0;
+		double symmetryScore		= 0;
+		for (Double pixelIntensity:quad1) {
+			Double rightPixelIntensity = quad2.get(pixelCounter);
+			double diffIntensity	= 1 - (Math.abs(pixelIntensity-rightPixelIntensity)/255.0);
+			diffIntensity			= diffIntensity/quad1.size();
+			symmetryScore	       += diffIntensity;
+//			System.out.println(pixelIntensity+"     "+rightPixelIntensity+" "+diffIntensity);
+			pixelCounter++;
+		}
+		return symmetryScore;
+	}
+	public double calculateLBPPixelValue() {
+		double middlePixelValue 	= pixelsUnderMask.get(Arrays.asList(0,0));
+		String c					= "";
+		for (ArrayList<Integer> pixel: pixelsUnderMask.keySet()) {
+			if(pixel.equals(Arrays.asList(0,0)))continue;
+			int newPixelValue		= 0;
+//			System.out.println(pixel);
+			float value = convolutionKernel.get(pixel);
+			float oldPixelValue = pixelsUnderMask.get(pixel);
+			if (oldPixelValue>=middlePixelValue) {
+				newPixelValue		= 1;
+			}
+			c						+=newPixelValue;
+			pixelsUnderMask.put(pixel, (float)newPixelValue);
+		}		
+		int decimalValue 			= Integer.parseInt(c, 2);
+		return decimalValue;
+	}
 	public double calculateNewPixelValue() {
 		double totalPixelValue	= 0.0f;
 		for (ArrayList<Integer> pixel: pixelsUnderMask.keySet()) {
@@ -69,15 +224,22 @@ public class ImageConvolutionMask {
 			totalPixelValue += newPixelValue;
 			pixelsUnderMask.put(pixel, newPixelValue);
 		}		
+//		totalPixelValue			= map(totalPixelValue,-1000,1000,0,255);
 		return totalPixelValue;
 	}
 	public Float getPixelValueFromImage (int absPosX, int absPosY) {
 		try {
-			if (absPosX>loadedImage.width() |absPosX<0) {
-				return 0.0f;
+			if (absPosX>=loadedImage.width()) {
+				absPosX = loadedImage.width()-1;
 			}
-			if (absPosY>loadedImage.height() |absPosY<0) {
-				return 0.0f;
+			if (absPosX <=0) {
+				absPosX	=0;
+			}
+			if (absPosY>=loadedImage.height()) {
+				absPosY = loadedImage.height()-1;
+			}
+			if (absPosY<=0) {
+				absPosY=0;
 			}
 			
 			double[] pixel = loadedImage.get(absPosY,absPosX);
@@ -86,7 +248,7 @@ public class ImageConvolutionMask {
 			return (float)loadedImage.get(absPosY,absPosX)[0];
 		}
 		catch (Exception ex) {
-//			System.out.println(absPosX + " NOT WORKING " + absPosY);
+			System.out.println(absPosX + " OutofBounds " + absPosY);
 //			System.out.println(ex);
 			return 0.0f;
 		}
